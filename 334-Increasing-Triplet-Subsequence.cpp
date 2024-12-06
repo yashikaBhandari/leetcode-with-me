9e24c33800286cf871ea9bb2acdccee26e914a41
yashika bhandari <120774542+yashikaBhandari@users.noreply.github.com>
2024-12-06 09:29:40 +0530

    [334] [Time Beats: 100.00%] [Memory Beats: 15.89%] - LeetPush

---
+++ 334-Increasing-Triplet-Subsequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        // o(n) solution
+        int n=nums.size();
+        int num1=INT_MAX;
+        int num2=INT_MAX;
+for(int i=0;i<n;i++){
+    int num3=nums[i];
+    if(num3<=num1){
+        num1=num3;
+    }
+    else if(num3<=num2){
+        num2=num3;
+    }
+    else{
+        return true;
+    }
+}
+return false;
+
+
+    }
+};
